14c8340f531927a1735ff3ce8243f7ac3902ecdb
dmitry.korniienko <dmitry.korniienko@akqa.com>
2013-02-20 17:23:46 +0200

    fixed call

--- dev/jQuery.vTimeline.js
+++ dev/jQuery.vTimeline.js
@@ -322,7 +322,7 @@
 	    }
 	};
 
-	$.fn.akqaTimelineV = function(method) {
+	$.fn.vTimeline = function(method) {
 	    if ( methods[method] ) {
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
 	    } else if ( typeof method === 'object' || ! method ) {
